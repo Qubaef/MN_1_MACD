b82df48d1fb202a3afe282c470a3c52fc6cd307e
Qubaef <qubaefg@gmail.com>
2020-03-03 21:00:47 +0100

    Mass import

--- MN_1_MACD.py
+++ MN_1_MACD.py
@@ -36,45 +36,70 @@ def signal_generate(macd):
     return ema_generate(macd, 9)
 
 
-day_diff = 2000             # get data from 2000 days
+day_diff = 365*20             # get data from 2000 days
 end = dt.date(2020, 2, 1)
 start = end - dt.timedelta(days = day_diff)
 
-length = 1000               # number of rows taken from downloaded data
+length = 365*20               # number of rows taken from downloaded data
 foldername = 'data'
-column = 'Open'             # name of the column, from which data is imported
-symbol = 'INTC'             # symbol of company whose data is downloaded
-filename = symbol + '.csv'
-data_source = 'yahoo'       # get stock data from 'Yahoo! Finance' acording to pandas_datareader docs
+column = 'Open'                 # name of the column, from which data is imported
+# symbol = ['F', 'AAPL', 'AMD', 'INTC', 'BAC', 'MSFT', 'NOK', 'UBER', 'AAL', 'GE', 'JD', 'FB', 'TWTR']                  # symbol of company whose data is downloaded
+symbol = ['FB']                  # symbol of company whose data is downloaded
+data_source = 'yahoo'           # get stock data from 'Yahoo! Finance' acording to pandas_datareader docs
+balances = []
 
-data = src.data.DataReader(symbol, data_source, start, end)[column]
-data.to_csv(foldername + '/' + filename)
-df = pd.read_csv(foldername + '/' + filename, header = 0, index_col = 'Date' , parse_dates = True).tail(length)
+for x in range(0, len(symbol)):
+    filename = symbol[x] + '.csv'
 
-
-#### calculate data
-# calculate macd
-macd = macd_generate(df[column])
-# calculate signal from macd
-signal = signal_generate(macd)
-# get array with 'column' data
-original = df[column].tolist()
-
-# calculate x axis to position figures properly
-x_axis = list(range(1,len(original) + 1))
-plt.plot(x_axis, original, 'g', label=column)
-
-#cut x_axis to move figures to the right
-x_axis = x_axis[26:]
-
-plt.plot(x_axis, macd, 'b', label="MACD")
-
-#cut x_axis to move figures to the right
-x_axis = x_axis[9:]
-plt.plot(x_axis, signal, 'r', label="SIGNAL")
-
-plt.legend()
-plt.show()
+    data = src.data.DataReader(symbol[x], data_source, start, end)[column]
+    data.to_csv(foldername + '/' + filename)
+    df = pd.read_csv(foldername + '/' + filename, header = 0, index_col = 'Date' , parse_dates = True).tail(length)
 
 
+    #### calculate data
+    # calculate macd
+    macd = macd_generate(df[column])
+    # calculate signal from macd
+    signal = signal_generate(macd)
+    macd = macd[9:]
+    # get array with 'column' data
+    original = df[column].tolist()
+    original = original[35:]
 
+    plt.plot(original, 'g', label=column)
+    plt.plot(macd, 'b', label="MACD")
+    plt.plot(signal, 'r', label="SIGNAL")
+    
+    plt.legend()
+    plt.show()
+
+    # cout profit from actions
+    balance = 1000
+    shares = 0
+
+    if macd[0] > signal[0]:
+        state = 1       # state=1 stands for 'over'
+    else:
+        state = 0       # state=0 stands for 'under'
+
+    for i in range(0, len(original)):
+        if(state == 1 and macd[i] < signal[i]):
+            # sell shares
+            balance += shares * original[i]
+            shares = 0
+            state = 0
+        elif state == 0 and macd[i] > signal[i]:
+            # buy shares
+            shares = int(balance/original[i])
+            balance -= original[i] * shares
+            state = 1
+    print('\nCompany: ' + symbol[x])
+    print('Balance: ' + str(balance) + '\nShares: ' + str(shares))
+    balance += shares * original[i]
+    print('Balance for last day ' + str(balance))
+    balances.append(balance)
+
+for i in range(0, len(balances)):
+    balance = balances[i] - 1000
+
+print('Total profit ' + str(balance))
